0b8e517b317e2a47344421a0d3a5de865929044c
Surajit <piklu3043@gmail.com>
2022-06-11 18:33:11 +0530

    Write some code to understand the sort() method

---
+++ app.js
@@ -0,0 +1,12 @@
+// const fruits = ['Banana', 'Apple', 'Orange', 'Mango'];
+// fruits.sort();
+// console.log(fruits);
+
+// const points = [42, 55, 11, 3, 95];
+// points.sort();
+// console.log(points);
+
+
+// const numbers = [42, 55, 11, 3, 95];
+// points.sort( (a, b) => a - b);  // array.sort(compareFunction)
+// console.log(numbers); 
