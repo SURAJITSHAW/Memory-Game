51f89632773023c67836c5cd1b05aba1df008bd3
Surajit <piklu3043@gmail.com>
2022-06-12 18:50:11 +0530

    created all 3 files necessary for the game, and add images too

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
-// const fruits = ['Banana', 'Apple', 'Orange', 'Mango'];
-// fruits.sort();
-// console.log(fruits);
-
-// const points = [42, 55, 11, 3, 95];
-// points.sort();
-// console.log(points);
-
-
-// const numbers = [42, 55, 11, 3, 95];
-// points.sort( (a, b) => a - b);  // array.sort(compareFunction)
-// console.log(numbers); 
+//card options
+const cardArray = [
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    }
+  ]
